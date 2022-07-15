eb4756bfa2e1009ced3f3ec0a8c681b46143cc46
sarahm-30 <100020901+sarahm-30@users.noreply.github.com>
2022-07-15 14:59:57 +0530

    Add files via upload

---
+++ task 4/src/App.js
@@ -0,0 +1,43 @@
+import Users from "./Components/card";
+import "./styles.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: true };
+    this.showUsers = this.showUsers.bind(this);
+  }
+
+  showUsers() {
+    document.getElementById("main").style.display = "flex";
+    const source = "https://reqres.in/api/users?page=1";
+    fetch(source)
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users_data: users.data, loading: false });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg navbar-dark  m-3">
+          <div className="container">
+            <span>
+              <h1>Let's Grow More</h1>
+            </span>
+            <button onClick={this.showUsers}>Get Users</button>
+          </div>
+        </nav>
+
+        <Users loading={this.state.loading} users={this.state.users_data} />
+      </>
+    );
+  }
+}
+
+export default App;
